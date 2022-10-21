2f87b99cb93dc5c4c50fdbd10a42d05b9e814b2f
Arka <arkac720@gmail.com>
2022-10-21 23:40:00 +0530

    first commit

---
+++ src/FirstScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './FirstScreen.css';
+
+ function FirstScreen() {
+    
+  return (
+    <div className='back'>
+        <img src="./images/image2.jpg" alt="" />
+    <div className='div'>
+      <div className='expertise'>EXPERTISE</div>
+      <div className='projects'>PROJECTS</div>
+      <div className='prime'>
+        <img src="./images/image.jpg" alt=""/>
+      </div>
+      <div className='invest'>INVEST</div>
+      <div className='housing'>HOUSING</div>
+      <div className='en'>Enquire</div>
+      <div className='rect1'></div>
+      <div className='rect2'></div>
+      <div className='rect3'></div>
+    </div>
+    <div className='front'>
+        <img src="./images/Rectangle65.jpg" alt=""/>
+    </div>
+    <div className='tribeout'>
+    <div className='tribe'>JOIN OUR TRIBE</div>
+    </div>
+    <div className='f58'>
+        <div className='tom'>
+        Let’s join our efforts towards building the unshakable foundation for a better tomorrow.
+        </div>
+    </div>
+    
+    </div>
+  )
+}
+
+export default FirstScreen;
